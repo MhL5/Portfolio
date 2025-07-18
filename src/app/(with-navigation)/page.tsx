f430e167de25a8ef5686a23d4e7ea8991fe5cb94
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-18 20:08:01 +0330

    Update animations in Home component and adjust global CSS timing for smoother transitions

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
         quantity={100}
       />
 
-      <div className="animate-fade-left via-muted hidden h-px w-dvw bg-gradient-to-r from-zinc-300/0 to-zinc-300/0 md:block" />
+      <div className="animate-fade-left via-muted h-px w-dvw bg-gradient-to-r from-zinc-300/0 to-zinc-300/0" />
       <h1 className="text-edge-outline animate-title bg-foreground font-space-grotesk z-10 cursor-default bg-clip-text px-0.5 py-3.5 text-4xl font-semibold whitespace-nowrap text-transparent duration-300 sm:text-6xl md:text-7xl lg:text-8xl">
         Mohammad Lashani
       </h1>
-      <div className="animate-fade-right via-muted hidden h-px w-screen bg-gradient-to-r from-zinc-300/0 to-zinc-300/0 md:block" />
+      <div className="animate-fade-right via-muted h-px w-screen bg-gradient-to-r from-zinc-300/0 to-zinc-300/0" />
 
-      <div className="animate-fade-in mt-3 text-center">
+      <div className="animate-fade-in mt-3 px-1 text-center">
         <h2 className="text-muted-foreground text-sm tracking-wide lg:text-base">
           I&apos;m a software engineer with a passion for building products that
           help people live better lives.
