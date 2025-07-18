d3ec9ad58a418bc8fd621dcea8d3dc2c0cadfb43
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-18 21:19:39 +0330

    Add Typography component with variant support for Tailwind CSS; update documentation to reflect new implementation.

---
+++ src/app/(with-navigation)/snippets/components/Typography/index.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+const typographyVariants = cva("text-xl", {
+  variants: {
+    variant: {
+      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+      h2: "scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0",
+      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+      p: "leading-7 [&:not(:first-child)]:mt-6",
+    },
+    effect: {
+      default: "",
+      lead: "text-muted-foreground text-xl",
+      large: "text-lg font-semibold",
+      small: "text-sm leading-none font-medium",
+      muted: "text-muted-foreground text-sm",
+      removePMargin: "[&:not(:first-child)]:mt-0",
+    },
+  },
+  defaultVariants: {
+    variant: "h1",
+    effect: "default",
+  },
+});
+
+export type TypographyProps = React.HTMLAttributes<HTMLHeadingElement> &
+  VariantProps<typeof typographyVariants>;
+
+export default function Typography({
+  className,
+  variant,
+  effect,
+  ...props
+}: TypographyProps) {
+  const Comp = variant || "p";
+
+  return (
+    <Comp
+      className={cn(typographyVariants({ variant, effect, className }))}
+      {...props}
+    />
+  );
+}
