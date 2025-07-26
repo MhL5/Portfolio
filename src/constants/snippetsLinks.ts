4fda1f39e9a904ad8980e0143984ccfada576532
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-27 01:27:36 +0330

    Add DrawerDialog component with responsive behavior; integrate vaul library for drawer functionality and update snippetsLinks to include DrawerDialog.

--- src/constants/snippetsLinks.ts
+++ src/constants/snippetsLinks.ts
@@ -12,155 +12,159 @@ type Links = {
 
 export const snippetsLinks: Links[] = [
   {
-    title: "components",
-    url: "/snippets/components",
-    items: [
+    "title": "components",
+    "url": "/snippets/components",
+    "items": [
       {
-        title: "ClientOnly",
-        url: "/snippets/components/ClientOnly",
+        "title": "ClientOnly",
+        "url": "/snippets/components/ClientOnly"
       },
       {
-        title: "FadeShadow",
-        url: "/snippets/components/FadeShadow",
+        "title": "DrawerDialog",
+        "url": "/snippets/components/DrawerDialog"
       },
       {
-        title: "Img",
-        url: "/snippets/components/Img",
+        "title": "FadeShadow",
+        "url": "/snippets/components/FadeShadow"
       },
       {
-        title: "InputWithIcon",
-        url: "/snippets/components/InputWithIcon",
+        "title": "Img",
+        "url": "/snippets/components/Img"
       },
       {
-        title: "MatchMedia",
-        url: "/snippets/components/MatchMedia",
+        "title": "InputWithIcon",
+        "url": "/snippets/components/InputWithIcon"
       },
       {
-        title: "ScrollToTopOnNavigation",
-        url: "/snippets/components/ScrollToTopOnNavigation",
+        "title": "MatchMedia",
+        "url": "/snippets/components/MatchMedia"
       },
       {
-        title: "StructuredDataScript",
-        url: "/snippets/components/StructuredDataScript",
+        "title": "ScrollToTopOnNavigation",
+        "url": "/snippets/components/ScrollToTopOnNavigation"
       },
       {
-        title: "Typography",
-        url: "/snippets/components/Typography",
+        "title": "StructuredDataScript",
+        "url": "/snippets/components/StructuredDataScript"
       },
-    ],
+      {
+        "title": "Typography",
+        "url": "/snippets/components/Typography"
+      }
+    ]
   },
   {
-    title: "hooks",
-    url: "/snippets/hooks",
-    items: [
+    "title": "hooks",
+    "url": "/snippets/hooks",
+    "items": [
       {
-        title: "useArray",
-        url: "/snippets/hooks/useArray",
+        "title": "useArray",
+        "url": "/snippets/hooks/useArray"
       },
       {
-        title: "useCopyToClipboard",
-        url: "/snippets/hooks/useCopyToClipboard",
+        "title": "useCopyToClipboard",
+        "url": "/snippets/hooks/useCopyToClipboard"
       },
       {
-        title: "useDebounce",
-        url: "/snippets/hooks/useDebounce",
+        "title": "useDebounce",
+        "url": "/snippets/hooks/useDebounce"
       },
       {
-        title: "useEventListener",
-        url: "/snippets/hooks/useEventListener",
+        "title": "useEventListener",
+        "url": "/snippets/hooks/useEventListener"
       },
       {
-        title: "useIsMobile",
-        url: "/snippets/hooks/useIsMobile",
+        "title": "useIsMobile",
+        "url": "/snippets/hooks/useIsMobile"
       },
       {
-        title: "useIsMounted",
-        url: "/snippets/hooks/useIsMounted",
+        "title": "useIsMounted",
+        "url": "/snippets/hooks/useIsMounted"
       },
       {
-        title: "useIsVisible",
-        url: "/snippets/hooks/useIsVisible",
+        "title": "useIsVisible",
+        "url": "/snippets/hooks/useIsVisible"
       },
       {
-        title: "useMediaQuery",
-        url: "/snippets/hooks/useMediaQuery",
+        "title": "useMediaQuery",
+        "url": "/snippets/hooks/useMediaQuery"
       },
       {
-        title: "useOnClickOutside",
-        url: "/snippets/hooks/useOnClickOutside",
+        "title": "useOnClickOutside",
+        "url": "/snippets/hooks/useOnClickOutside"
       },
       {
-        title: "usePrevious",
-        url: "/snippets/hooks/usePrevious",
+        "title": "usePrevious",
+        "url": "/snippets/hooks/usePrevious"
       },
       {
-        title: "useSize",
-        url: "/snippets/hooks/useSize",
+        "title": "useSize",
+        "url": "/snippets/hooks/useSize"
       },
       {
-        title: "useStateWithHistory",
-        url: "/snippets/hooks/useStateWithHistory",
+        "title": "useStateWithHistory",
+        "url": "/snippets/hooks/useStateWithHistory"
       },
       {
-        title: "useStateWithValidation",
-        url: "/snippets/hooks/useStateWithValidation",
+        "title": "useStateWithValidation",
+        "url": "/snippets/hooks/useStateWithValidation"
       },
       {
-        title: "useStorage",
-        url: "/snippets/hooks/useStorage",
+        "title": "useStorage",
+        "url": "/snippets/hooks/useStorage"
       },
       {
-        title: "useTimeout",
-        url: "/snippets/hooks/useTimeout",
+        "title": "useTimeout",
+        "url": "/snippets/hooks/useTimeout"
       },
       {
-        title: "useToggleState",
-        url: "/snippets/hooks/useToggleState",
-      },
-    ],
+        "title": "useToggleState",
+        "url": "/snippets/hooks/useToggleState"
+      }
+    ]
   },
   {
-    title: "types",
-    url: "/snippets/types",
-    items: [
+    "title": "types",
+    "url": "/snippets/types",
+    "items": [
       {
-        title: "AutoComplete",
-        url: "/snippets/types/AutoComplete",
+        "title": "AutoComplete",
+        "url": "/snippets/types/AutoComplete"
       },
       {
-        title: "LaravelPaginatedResourceType",
-        url: "/snippets/types/LaravelPaginatedResourceType",
+        "title": "LaravelPaginatedResourceType",
+        "url": "/snippets/types/LaravelPaginatedResourceType"
       },
       {
-        title: "PartialPick",
-        url: "/snippets/types/PartialPick",
+        "title": "PartialPick",
+        "url": "/snippets/types/PartialPick"
       },
       {
-        title: "RequiredPick",
-        url: "/snippets/types/RequiredPick",
-      },
-    ],
+        "title": "RequiredPick",
+        "url": "/snippets/types/RequiredPick"
+      }
+    ]
   },
   {
-    title: "utils",
-    url: "/snippets/utils",
-    items: [
+    "title": "utils",
+    "url": "/snippets/utils",
+    "items": [
       {
-        title: "groupByKey",
-        url: "/snippets/utils/groupByKey",
+        "title": "groupByKey",
+        "url": "/snippets/utils/groupByKey"
       },
       {
-        title: "isServer",
-        url: "/snippets/utils/isServer",
+        "title": "isServer",
+        "url": "/snippets/utils/isServer"
       },
       {
-        title: "objectKeysTyped",
-        url: "/snippets/utils/objectKeysTyped",
+        "title": "objectKeysTyped",
+        "url": "/snippets/utils/objectKeysTyped"
       },
       {
-        title: "stringFormatters",
-        url: "/snippets/utils/stringFormatters",
-      },
-    ],
-  },
+        "title": "stringFormatters",
+        "url": "/snippets/utils/stringFormatters"
+      }
+    ]
+  }
 ];
