f5bbabf7b71ff5d74a0777fa2ccfbfe0a26eca03
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-08-18 11:58:20 +0330

    Add useOtp hook and corresponding documentation: implement useOtp hook for OTP retrieval from user device, create MDX documentation for usage and installation, and add JSON schema for registry integration.

---
+++ src/registry/hooks/useOtp/useOtp.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+type otpContent = CredentialRequestOptions & {
+  otp: { transport: ["sms"] };
+};
+
+export function useOtp(callback: (code: string) => void) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    if (!("OTPCredential" in window)) return;
+    const abortController = new AbortController();
+
+    async function getOTP() {
+      try {
+        const content = await navigator.credentials.get({
+          otp: { transport: ["sms"] },
+          signal: abortController.signal,
+        } as otpContent);
+
+        if (content && "code" in content && typeof content.code === "string")
+          callbackRef.current(content.code);
+      } catch {}
+    }
+    getOTP();
+
+    return () => abortController.abort();
+  }, []);
+}
