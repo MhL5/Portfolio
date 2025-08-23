bfc776b88165baf4b32483e4803c8b68b388c2ef
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-08-23 15:42:12 +0330

    Update Footer component styles: Adjust padding and text alignment for improved layout and readability in the footer section.

--- src/app/(with-navigation)/_components/Footer.tsx
+++ src/app/(with-navigation)/_components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="border-border/50 flex h-13 items-center border-t">
-      <div className="mx-auto w-full max-w-7xl text-sm">
+    <footer className="border-border/50 flex h-13 items-center border-t px-5">
+      <div className="mx-auto w-full max-w-7xl text-center text-sm sm:text-start">
         Built by
         <Button asChild variant="link" className="h-fit px-1 underline">
           <Link href="https://github.com/MhL5">Mohammad Lashani</Link>
