0e4c51a8e83624e5e9651d995869995d945598e9
MhL5 <sapumr@gmail.com>
2025-10-05 17:31:13 +0330

    chore: remove useKey hook and related files; update README with carousel link

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ todos:
 - add metadata
 - add auto grid and error catchers
 - check todos folder and remove useless ones
+
+https://www.youtube.com/shorts/uA_NEqFatfU add simple native carousel
--- src/features/MDX-remote/components/CodePreview.tsx
+++ src/features/MDX-remote/components/CodePreview.tsx
@@ -33,7 +33,6 @@ const CodePreviewSources = {
   MatchMedia: "new-york/MatchMedia/example.tsx",
   useCopyToClipboard: "hooks/useCopyToClipboard/example.tsx",
   useIsMounted: "hooks/useIsMounted/example.tsx",
-  useKey: "hooks/useKey/example.tsx",
   useUrlState: "hooks/useUrlState/example.tsx",
   AutoGrid: "new-york/AutoGrid/example.tsx",
   FallbackPagesError: "new-york/FallbackPages/ErrorExample.tsx",
