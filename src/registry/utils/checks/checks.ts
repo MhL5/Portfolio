aa7dd0dc4c5170e383258890c7035bf52ab40fd5
MhL5 <sapumr@gmail.com>
2025-12-11 13:16:13 +0330

    refactor: simplify tryCatch example and rename isDOMAvailable to isBrowser for improved clarity and consistency

--- src/registry/utils/checks/checks.ts
+++ src/registry/utils/checks/checks.ts
@@ -1,7 +1,6 @@
 export const isServer = () => typeof window === "undefined";
 export const isClient = () => typeof window !== "undefined";
-
-export const isDOMAvailable = () => typeof document !== "undefined";
+export const isBrowser = () => typeof document !== "undefined";
 
 export const isDev = () => process.env.NODE_ENV === "development";
 export const isProd = () => process.env.NODE_ENV === "production";
