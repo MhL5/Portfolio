ddceca2a9b46dbd9685c2e23c06139690f7c6b3b
MhL5 <sapumr@gmail.com>
2025-12-11 13:19:07 +0330

    refactor: remove unused dev:webpack script from package.json and update useOnClickOutside hook to use isBrowser for improved clarity

--- src/registry/hooks/useOnClickOutside/useOnClickOutside.ts
+++ src/registry/hooks/useOnClickOutside/useOnClickOutside.ts
@@ -2,13 +2,13 @@
 
 import type { RefObject } from "react";
 import useEventListener from "@/registry/hooks/useEventListener/useEventListener";
-import { isDOMAvailable } from "@/registry/utils/checks/checks";
+import { isBrowser } from "@/registry/utils/checks/checks";
 
 export default function useOnClickOutside(
   ref: RefObject<HTMLElement | null>,
   cb: (e: MouseEvent) => void,
 ) {
-  const element = isDOMAvailable() ? document : undefined;
+  const element = isBrowser() ? document : undefined;
 
   useEventListener(
     "click",
